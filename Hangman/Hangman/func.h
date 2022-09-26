ee1f10819c8d0d21b4877ea8897abf9496f1bc90
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-26 11:58:03 +0700

    Commit 3
    Create func.h
    Add function to load word from file

---
+++ Hangman/Hangman/func.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <io.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys\locking.h>
+#include <string.h>
+#include <ctype.h>
+#define max_length 21
+
+//функция загрузки слова из файла words.txt
+bool loadWord(FILE * file, char* word) {
+	int i = 0;
+	char s[max_length]{ 0 };
+	static int count = -1;
+// подсчет количества слов в файле
+	if (count == -1) {
+		while (!feof(file)) {
+			fgets(s, max_length, file);
+			count++;
+		}
+		if (count == 0)
+			return false;
+		fseek(file, 0, 0);
+	}
+// выбираем случайное слово для игры
+	int r = rand() % count;
+	while (i <= r) {
+		fgets(s, max_length, file);
+		i++;
+	}
+// определяем длину слова и копируем его
+	int wordlen = strlen(s);
+	if (s[wordlen - 1] == 10)
+		s[wordlen - 2] = 0;
+	strcpy(word, s);
+// блокировка файла
+	int hFile = _fileno(file);
+	int size = _filelength(hFile);
+	fseek(file, 0, 0);
+	_locking(hFile, _LK_NBLCK, size);
+
+	return true;
+
+}
--- Hangman/Hangman/main.cpp
+++ Hangman/Hangman/main.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<windows.h>
+#include<time.h>
+#include<string.h>
+#include"func.h"
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}

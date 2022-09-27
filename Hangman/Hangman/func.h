c147f852736965bf18bc805c016d00ac82476ffb
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-27 12:40:37 +0700

    Commit 4
    Add function Game

--- Hangman/Hangman/func.h
+++ Hangman/Hangman/func.h
@@ -10,6 +10,10 @@
 #include <ctype.h>
 #define max_length 21
 
+// переменные для количества попыток и количества угаданных слов
+int Tries{ 10 };
+int CountWords{ 0 };
+
 //функция загрузки слова из файла words.txt
 bool loadWord(FILE * file, char* word) {
 	int i = 0;
@@ -44,4 +48,84 @@ bool loadWord(FILE * file, char* word) {
 
 	return true;
 
+}
+// функция начала игры
+void Game(char* word) {
+// переводим слово в верхний регистры
+	_strupr(word);
+	int l = strlen(word);
+// строка копия для отображения
+	char* copy = new char[l + 1];
+	memset(copy, '*', l);
+	copy[l] = 0;
+//	алфавит для отображения в консоли
+	char letters[52]{};
+	int i, j;
+	for (i = 0; i < 26; i++) {
+		letters[j++] = i + 'A';
+		letters[j++] = ' ';
+	}
+	letters[51] = 0;
+
+	char letter;
+	char* pos;
+	bool replace = false;
+
+	do {
+
+		system("cls");
+		cout << copy << endl << endl;
+		cout << letters << endl << endl;
+		cout << "Count of tries: " << Tries << endl
+			<< endl;
+		cout << "Input any letter:\t";
+		cin >> letter;
+// проверка нажата ли буква
+		if (!isalpha(letter)) {
+			cout << "It's not a letter" << endl;
+			Sleep(1000);
+			continue;
+		}
+		letter = toupper(letter);
+// поиск буквы в алфавите		
+		pos = strchr(letters, letter);
+
+		if (pos == 0)
+		{
+			cout << "This letter have been already pressed" << endl;
+			Sleep(1000);
+			continue;
+		}
+		else
+		{
+			pos[0] = ' ';
+		}
+// поиск буквы в загаданном слове
+		for (i = 0; i < l; i++)
+		{
+			if (word[i] == letter)
+			{
+				copy[i] = letter;
+				replace = true;
+			}
+		}
+		if (replace == false)
+			Tries--;
+		else
+			replace = false;
+// вывод при победе на экран
+		if (strcmp(word, copy) == 0)
+		{
+			system("cls");
+			cout << copy << endl << endl;
+			cout << letters << endl << endl;
+			cout << "Count of tries: " << Tries <<
+				endl << endl;
+			cout << "Congratulation !!!" << endl;
+			CountWords++;
+			break;
+		}
+	}
+	while(Tries != 0);
+	delete[] copy;
 }
